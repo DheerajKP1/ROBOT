fab43f09b396be29fa7167fbccbaca33e17bff29
Dheeraj <dheerajpra04@gmail.com>
2025-09-13 01:54:18 +0530

    updated

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -1,137 +1,121 @@
 import os
-import xacro
+
 from ament_index_python.packages import get_package_share_directory
+
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
 
 
-def generate_launch_description():
-    # Define the robot's name and package name
-    robot_name = "differential_drive_robot"
-    package_name = "mtp_robot"
 
-    # Define a launch argument for the world file, defaulting to "empty.sdf"
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    
+    package_name='mtp_robot' #<--- CHANGE ME
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
+
+
+
+
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'empty.world'
+        )    
+    
+    world = LaunchConfiguration('world')
+
     world_arg = DeclareLaunchArgument(
         'world',
-        default_value='empty.sdf',
-        description='Specify the world file for Gazebo (e.g., empty.sdf)'
-    )
-
-    # Define launch arguments for initial pose
-    x_arg = DeclareLaunchArgument(
-        'x', default_value='0.0', description='Initial X position')
-
-    y_arg = DeclareLaunchArgument(
-        'y', default_value='0.0', description='Initial Y position')
-
-    z_arg = DeclareLaunchArgument(
-        'z', default_value='0.5', description='Initial Z position')
-
-    roll_arg = DeclareLaunchArgument(
-        'R', default_value='0.0', description='Initial Roll')
-
-    pitch_arg = DeclareLaunchArgument(
-        'P', default_value='0.0', description='Initial Pitch')
-
-    yaw_arg = DeclareLaunchArgument(
-        'Y', default_value='0.0', description='Initial Yaw')
-
-    # Retrieve launch configurations
-    world_file = LaunchConfiguration('world')
-    x = LaunchConfiguration('x')
-    y = LaunchConfiguration('y')
-    z = LaunchConfiguration('z')
-    roll = LaunchConfiguration('R')
-    pitch = LaunchConfiguration('P')
-    yaw = LaunchConfiguration('Y')
-
-    # Set paths to Xacro model and configuration files
-    robot_model_path = os.path.join(
-        get_package_share_directory(package_name),
-        'model',
-        'robot.urdf.xacro'
-    )
-
-    gz_bridge_params_path = os.path.join(
-        get_package_share_directory(package_name),
-        'config',
-        'gz_bridge.yaml'
-    )
-
-    # Process the Xacro file to generate the URDF representation of the robot
-    robot_description = xacro.process_file(robot_model_path).toxml()
-
-    # Prepare to include the Gazebo simulation launch file
-    gazebo_pkg_launch = PythonLaunchDescriptionSource(
-        os.path.join(
-            get_package_share_directory('ros_gz_sim'),
-            'launch',
-            'gz_sim.launch.py'
+        default_value=default_world,
+        description='World to load'
         )
+
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+             )
+
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'my_bot',
+                                   '-z', '0.1'],
+                        output='screen')
+
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
     )
 
-    # Include the Gazebo launch description with specific arguments
-    gazebo_launch = IncludeLaunchDescription(
-        gazebo_pkg_launch,
-        launch_arguments={
-            'gz_args': [f'-r -v 4 ', world_file],
-            'on_exit_shutdown': 'true'
-        }.items()
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
     )
 
-    # Create a node to spawn the robot model in the Gazebo environment
-    spawn_model_gazebo_node = Node(
-        package='ros_gz_sim',
-        executable='create',
+
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
         arguments=[
-            '-name', robot_name,
-            '-string', robot_description,
-            '-x', x,
-            '-y', y,
-            '-z', z,
-            '-R', roll,
-            '-P', pitch,
-            '-Y', yaw,
-            '-allow_renaming', 'false'
-        ],
-        output='screen',
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
     )
 
-    # Create a node to publish the robot's state based on its URDF description
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[
-            {'robot_description': robot_description, 'use_sim_time': True}
-        ],
-        output='screen'
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
     )
 
-    # Create a node for the ROS-Gazebo bridge to handle message passing
-    gz_bridge_node = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=[
-            '--ros-args', '-p',
-            f'config_file:={gz_bridge_params_path}'
-        ],
-        output='screen'
-    )
 
+
+    # Code for delaying a node (I haven't tested how effective it is)
+    # 
+    # First add the below lines to imports
+    # from launch.actions import RegisterEventHandler
+    # from launch.event_handlers import OnProcessExit
+    #
+    # Then add the following below the current diff_drive_spawner
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[diff_drive_spawner],
+    #     )
+    # )
+    #
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+
+
+
+    # Launch them all!
     return LaunchDescription([
+        rsp,
         world_arg,
-        gazebo_launch,
-        x_arg,
-        y_arg,
-        z_arg,
-        roll_arg,
-        pitch_arg,
-        yaw_arg,
-        spawn_model_gazebo_node,
-        robot_state_publisher_node,
-        gz_bridge_node,
+        gazebo,
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner,
+        ros_gz_bridge,
+        ros_gz_image_bridge
     ])
--- readme.md
+++ readme.md
@@ -3,7 +3,11 @@ ros2 run teleop_twist_keyboard teleop_twist_keyboard
 
 
 
-colcon build
-source install/setup.bash
+
+colcon build 
+source install/setup.bash 
 ros2 launch mtp_robot robot.launch.py
+ros2 run rviz2 rviz2
+
+
 
